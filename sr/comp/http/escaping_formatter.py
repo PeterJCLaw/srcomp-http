ffc280aeaf68482848f1de8f0f9c8db0f0675403
Peter Law <PeterJCLaw@gmail.com>
2020-03-08 22:01:02 +0000

    Explain why we're not going to rename this method

--- sr/comp/http/escaping_formatter.py
+++ sr/comp/http/escaping_formatter.py
@@ -5,7 +5,7 @@ import logging
 class EscapingFormatter(logging.Formatter):
     """A class that formats log output nicely."""
 
-    def format(self, record):
+    def format(self, record):  # noqa:A003 # name `format` is defined by parent class
         msg = super().format(record)
         # Newlines are the only thing known to have caused issues thus far
         escaped = msg.replace('\n', '\\n')
