9028685e23fac7129356831e027732b431aab7de
Peter Law <PeterJCLaw@gmail.com>
2023-04-15 18:31:35 +0100

    Don't override the docs version
    It would be nice if this could be pulled from the tag or setup.py
    or something, but at least omitting it is less wrong.

--- docs/conf.py
+++ docs/conf.py
@@ -17,9 +17,6 @@ master_doc = 'index'
 project = 'sr.comp.http'
 copyright = '2015, Student Robotics'
 
-release = '1.0.0'
-version = '1.0.0'
-
 html_theme = 'alabaster'
 
 intersphinx_mapping = {'http://docs.python.org/': None}
